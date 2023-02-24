022a78e59286997427c24b19872efe6a4bbb7327
c4nnh <cannh.99@gmail.com>
2023-02-24 21:36:44 +0700

    add api tag

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('Health check')
 export class AppController {
   @Get('health-check')
   healthCheck() {
